e4b1a583adf3cba25f2ff0fadc32d490caa9ea4d
codestudent <codestudent@708S-G103-231.canyonsdistrict.pvt>
2021-06-21 10:19:12 -0600

    Initial commit

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div className='app'>
+        <h1>Jordan Hudgens is a Cyborg</h1>
+        <h2>Bottom Text</h2>
+      </div>
+    );
+  }
+}
