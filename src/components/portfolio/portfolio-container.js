e81aeffcb3fa4d7322c9fd26ce851eee44d8d7d6
henryZheng727 <41962705+henryZheng727@users.noreply.github.com>
2021-06-25 19:44:08 -0600

    Add files via upload

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from 'axios';
 
 import PortfolioItem from "./portfolio-item";
 
@@ -9,12 +10,7 @@ export default class PortfolioContainer extends Component {
         this.state = {
             pageTitle: "Welcome to my portfolio",
             isLoading: false,
-            data: [
-                { title: "Quip", category: "eCommerce" },
-                { title: "Eventbrite", category: "Scheduling" },
-                { title: "Ministry Safe", category: "Enterprise" },
-                { title: "SwingAway", category: "eCommerce" }
-            ]
+            data: []
         };
 
         this.handleFilter = this.handleFilter.bind(this);
@@ -28,12 +24,34 @@ export default class PortfolioContainer extends Component {
         })
     }
 
+    getPortfolioItems() {
+        axios.get('https://henryzheng727.devcamp.space/portfolio/portfolio_items')
+            .then(response => {
+                this.setState({
+                    data: response.data.portfolio_items
+                })
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+
     portfolioItems() {
+
         return this.state.data.map(item => {
-            return <PortfolioItem title={item.title} url={"google.com"} />;
+            debugger;
+            return (
+                <PortfolioItem
+                    key={item.id}
+                    item={item}
+                />
+            );
         })
     }
 
+    componentDidMount() {
+        this.getPortfolioItems();
+    }
 
     render() {
         if (this.state.isLoading) {
