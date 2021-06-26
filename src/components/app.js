bfd81e6a544c75b753e32f471ed44b591fa5bad2
henryZheng727 <41962705+henryZheng727@users.noreply.github.com>
2021-06-25 19:44:33 -0600

    Add files via upload

--- src/components/app.js
+++ src/components/app.js
@@ -10,6 +10,10 @@ import PortfolioContainer from './portfolio/portfolio-container';
 import NavigationContainer from "./navigation/navigation-container";
 import Home from "./pages/home";
 import About from "./pages/about";
+import Contact from "./pages/contact";
+import Blog from "./pages/blog";
+import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -17,16 +21,20 @@ export default class App extends Component {
       <div className='app'>
         <Router>
           <div>
+            <h1>Henry Zheng Portfolio</h1>
+            <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
             <NavigationContainer />
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/about-me" component={About} />
+              <Route path="/contact" component={Contact} />
+              <Route path="/blog" component={Blog} />
+              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
-        <h1>Henry Zheng Portfolio</h1>
-        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
-        <PortfolioContainer />
+
       </div>
     );
   }
