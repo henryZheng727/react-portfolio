d76372eeefd0b0983bc410e1982285b822c4234a
codestudent <codestudent@708S-G103-231.canyonsdistrict.pvt>
2021-06-29 11:49:25 -0600

    added authentication and fixed bugs

--- src/components/auth/login.js
+++ src/components/auth/login.js
@@ -1,33 +1,62 @@
-//as of Introduction to React Forms, i cannot figure this out
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from 'axios';
 
 export default class Login extends Component {
     constructor(props) {
         super(props);
 
-        this.state =  {
+        this.state = {
             email: "",
-            password: ""
-        }
+            password: "",
+            errorText: "",
+        };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     handleChange(event) {
-        console.log("handle change", event);
+        this.setState({
+            [event.target.name]: event.target.value,
+            errorText: ""
+        });
     }
 
     handleSubmit(event) {
-        this.setState({
-            [event.target.name]: event.target.value
+        axios.post("https://api.devcamp.space/sessions",
+            {
+                client: {
+                    email: this.state.email,
+                    password: this.state.password
+                }
+            },
+            { withCredentials: true }
+        ).then(response => {
+            if (response.data.status === 'created') {
+                this.props.handleSuccessfulAuth();
+            } else {
+                this.setState({
+                    errorText: "Wrong email or password"
+                });
+                this.props.handleUnsuccessfulAuth();
+            }
+        }).catch(error => {
+            this.setState({
+                errorText: "An error occurred"
+            });
+            this.props.handleUnsuccessfulAuth();
         });
+
+        event.preventDefault();
     }
 
     render() {
         return (
             <div>
                 <h1>LOGIN TO ACCESS YOUR DASHBOARD</h1>
+
+                <div>{this.state.errorText}</div>
+
                 <form onSubmit={this.handleSubmit}>
                     <input
                         type="email"
