b57ae998dd917ac9ffe2a53b1279e0c1b08a0db7
codestudent <codestudent@708S-G103-231.canyonsdistrict.pvt>
2021-07-12 11:59:48 -0600

    added modal, boilerplate for blog form

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -6,6 +6,7 @@ import { Link } from "react-router-dom";
 import axios from 'axios';
 import BlogItem from "../blog/blog-item";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import BlogModal from "../modals/blog-modal";
 
 class Blog extends Component {
     constructor() {
@@ -15,12 +16,27 @@ class Blog extends Component {
             blogItems: [],
             totalCount: 0,
             currentPage: 0,
-            isLoading: true
-        }
+            isLoading: true,
+            blogModalIsOpen: false
+        };
 
         this.getBlogItems = this.getBlogItems.bind(this);
         this.onScroll = this.onScroll.bind(this);
         window.addEventListener("scroll", this.onScroll, false);
+        this.handleNewBlogClick = this.handleNewBlogClick.bind(this);
+        this.handleModalClose = this.handleModalClose.bind(this);
+    }
+
+    handleModalClose() {
+        this.setState({
+            blogModalIsOpen: false
+        })
+    }
+
+    handleNewBlogClick() {
+        this.setState({
+            blogModalIsOpen: true,
+        });
     }
 
     onScroll() {
@@ -73,6 +89,15 @@ class Blog extends Component {
 
         return (
             <div className="blog-container">
+                <BlogModal
+                    handleModalClose={this.handleModalClose}
+                    modalIsOpen={this.state.blogModalIsOpen}
+                />
+
+                <div className="new-blog-link">
+                    <a onClick={this.handleNewBlogClick}>Open Modal</a>
+                </div>
+
                 <div className="content-container">
                     {blogRecords}
                 </div>
